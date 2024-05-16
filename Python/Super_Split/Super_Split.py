5a4f53ec01c728b2d8221b70af6e2a573940647e
vvithershins <146913240+vvithershins@users.noreply.github.com>
2024-05-16 02:05:24 -0400

    Create Super_Split.py

---
+++ Python/Super_Split/Super_Split.py
@@ -0,0 +1,25 @@
+def super_split(text,sep,max):
+  if max < 0:
+    if abs(max) > text.count(sep):
+      max = -text.count(sep)
+    reversedword = reversed(text)
+    reversedword = "".join(reversedword)
+    new = reversedword.split(sep,abs(max))
+    new = " ".join(new)#uses non breaking space alt numpad 254
+    forward = reversed(new)
+    how = "".join(forward)
+    final = how.split(" ")#uses non breaking space alt numpad 254
+    
+  elif max > 0:
+    final = text.split(sep,max)
+    
+  else:
+    final = "There was an error, Cannot split 0 times"
+  return final
+
+  
+
+#Example
+text = "abc.def.ghi.jkl.mno"
+
+print(super_split(text,".",-2))
